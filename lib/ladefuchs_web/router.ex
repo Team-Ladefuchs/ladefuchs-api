20f957193d35003525a00ac4c5e869dfef5b7ae1
Stefan Fochler <istefo@me.com>
2021-09-02 21:03:04 +0200

    Router: add apidocs endpoint (no content yet)

--- lib/ladefuchs_web/router.ex
+++ lib/ladefuchs_web/router.ex
@@ -5,8 +5,21 @@ defmodule LadefuchsWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :apidocs do
+    plug Plug.Static,
+      at: "/apidocs",
+      brotli: true,
+      from: {:service_feeds, "priv/static/apidocs"}
+  end
+
   scope "/api", LadefuchsWeb do
     pipe_through :api
+  end
+
+  scope "/apidocs", LadefuchsWeb do
+    pipe_through :apidocs
+
+    get "/*path", Redirect, to: "/apidocs/index.html"
   end
 
   # Enables LiveDashboard only for development
