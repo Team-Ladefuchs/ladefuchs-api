1036e6b23955c1e8e33bdb004381c9fabb90cf74
DominicWrege <email@dwrege.de>
2023-05-18 21:59:56 +0200

    fix: search
    - because some operators have the same network
       - workaround: also check the name

--- sql/get/cpo/search_cache.sql
+++ sql/get/cpo/search_cache.sql
@@ -4,5 +4,5 @@ select cpo_cache.id,
         cpo_cache.updated,
         url,
         cpo.id as "cpo_id?"
-from cpo_cache left join cpo on cpo_cache.network = cpo.network
+from cpo_cache left join cpo on cpo_cache.network = cpo.network and lower(cpo_cache.slug_name) = lower(cpo.slug_name)
 where search @@ websearch_to_tsquery($1) or strict_word_similarity($1, cpo_cache.slug_name) > 0.25
