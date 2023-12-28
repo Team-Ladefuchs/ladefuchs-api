92163313b74d9caea78ee92951f6567a068acdf7
Dominic <email@dwrege.de>
2023-12-28 15:19:06 +0100

    build oci image using nix

---
+++ nix/flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "Ladefuchs API";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        inherit (pkgs) lib dockerTools;
+
+        craneLib = crane.lib.${system};
+
+        sqlFilter = path: _type: null != builtins.match ".*(sql|json)$" path;
+        sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
+
+        src = lib.cleanSourceWith {
+          src = craneLib.path ../.; # The original, unfiltered source
+          filter = sqlOrCargo;
+        };
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+
+        };
+
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        ladefuchs-api = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          nativeBuildInputs = (commonArgs.nativeBuildInputs or [ ]) ++ [
+            pkgs.sqlx-cli
+          ];
+
+          preBuild = ''
+			export SQLX_OFFLINE=true
+          '';
+        });
+
+		image = dockerTools.buildImage {
+			name = "ladefuchs-api";
+			tag = "latest";
+			config = {
+				Cmd = [ "${ladefuchs-api}/bin/ladefuchs-api" ];
+			};
+		};
+      in
+      {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit ladefuchs-api;
+        };
+
+        packages = {
+          default = ladefuchs-api;
+          inherit ladefuchs-api image;
+        };
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+          packages = [
+            pkgs.sqlx-cli
+          ];
+        };
+      });
+}
