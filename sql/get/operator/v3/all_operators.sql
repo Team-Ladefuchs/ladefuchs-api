dab6b218ee49ae784602d2a7f7e591fd73a58d18
Dominic <email@dwrege.de>
2024-08-11 22:40:41 +0200

    return tariffs and operator which have prices

--- sql/get/operator/v3/all_operators.sql
+++ sql/get/operator/v3/all_operators.sql
@@ -16,5 +16,5 @@ where
         where
             operator_id = operator.id and ccs_plug_count > 0
             or type2_plug_count > 0
-    )
+    ) and STRICT_WORD_SIMILARITY('tesla', slug_name) < 0.2
 order by operator.name
--- sql/get/tariff/v3/tariff_all.sql
+++ sql/get/tariff/v3/tariff_all.sql
@@ -6,18 +6,19 @@ SELECT
     monthly_fee,
     note,
     url AS affiliate_link_url,
+    tariff.updated AS last_updated_date,
     CASE
         WHEN image.soft_delete = false THEN $1 || 'image/' || image.checksum
-        ELSE null
     END AS image_url,
     CASE
         WHEN tariff.override_standard = true THEN tariff.override_standard
         ELSE tariff.standard
-    END AS "is_standard!",
-    tariff.updated AS last_updated_date
+    END AS "is_standard!"
 FROM
     tariff
 LEFT JOIN image ON tariff.image = image.id
-WHERE hide = false
+WHERE
+    hide = false
+    AND EXISTS (SELECT tariff_id FROM charge_price WHERE tariff_id = tariff.id)
 ORDER BY
     slug_name, provider_name
