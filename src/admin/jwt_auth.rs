0f25f0245883d301591da4b1ab35fe371217c051
Dominic <email@dwrege.de>
2024-10-19 18:18:58 +0200

    set cookie policy to lax

--- src/admin/jwt_auth.rs
+++ src/admin/jwt_auth.rs
@@ -138,7 +138,7 @@ pub async fn login(
                         .unwrap_or_default(),
                 )
                 .path("/")
-                .same_site(SameSite::Strict)
+                .same_site(SameSite::Lax)
                 .secure(cookie_secure())
                 .expires(expire)
                 .http_only(false)
