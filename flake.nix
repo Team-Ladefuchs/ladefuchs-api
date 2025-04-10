ea062da216dd43a8e6258993b440fe51a695cd60
Dominic <email@dwrege.de>
2025-04-10 22:54:33 +0200

    use direnv flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Dev shell with sqlx and podman-compose";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.sqlx-cli
+          pkgs.podman-compose
+        ];
+        shellHook = ''
+          if ! podman ps | grep -q postgres; then
+            podman-compose --file $(pwd)/docker-compose/docker-compose.yml up -d
+            echo "Started postgres"
+          fi
+          echo "Ready"
+        '';
+      };
+    };
+}
