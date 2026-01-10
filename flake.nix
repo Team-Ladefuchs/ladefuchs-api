d5175b130f35da7de4654b3008616ea84ad0b151
Dominic <email@dwrege.de>
2026-01-10 18:54:12 +0100

    update dev shell

--- flake.nix
+++ flake.nix
@@ -11,16 +11,18 @@
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.sqlx-cli
-          pkgs.podman-compose
+        buildInputs = with pkgs; [
+          sqlx-cli
+          podman-compose
+          cargo
+          rustc
         ];
         shellHook = ''
           if ! podman ps | grep -q postgres; then
             podman-compose --file $(pwd)/docker-compose/docker-compose.yml up -d
             echo "Started postgres"
           fi
-          echo "Ready"
+          echo "api dev ready"
         '';
       };
     };
