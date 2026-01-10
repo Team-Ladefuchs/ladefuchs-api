79bc0cac74f05b36decae859d992d9d2571179fd
Dominic <email@dwrege.de>
2026-01-10 19:01:49 +0100

    only start podman on linux

--- flake.nix
+++ flake.nix
@@ -6,24 +6,49 @@
   outputs =
     { nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          sqlx-cli
-          podman-compose
-          cargo
-          rustc
-        ];
-        shellHook = ''
-          if ! podman ps | grep -q postgres; then
-            podman-compose --file $(pwd)/docker-compose/docker-compose.yml up -d
-            echo "Started postgres"
-          fi
-          echo "api dev ready"
-        '';
-      };
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          isLinux = pkgs.stdenv.isLinux;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs =
+              with pkgs;
+              [
+                sqlx-cli
+                cargo
+                rustc
+              ]
+              ++ pkgs.lib.optionals isLinux [
+                podman-compose
+              ];
+
+            shellHook = ''
+              echo "api dev ready"
+
+              ${pkgs.lib.optionalString isLinux ''
+                if command -v podman > /dev/null; then
+                  if ! podman ps | grep -q postgres; then
+                    podman-compose \
+                      --file $(pwd)/docker-compose/docker-compose.yml up -d
+                    echo "Started postgres"
+                  fi
+                fi
+              ''}
+            '';
+          };
+        }
+      );
     };
 }
